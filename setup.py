769946d2a058e96c6809e53e200f69de7e93e6d9
samuelemwangi <mwangi.s.irungu@gmail.com>
2024-03-16 01:12:45 +0300

    fix file naming

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 VERSION = "0.0.1"
 DESCRIPTION = "Library to fetch StackOverflow data and persist it to an SQL database"
-LONG_DESCRIPTION = open("README.md").read()
+LONG_DESCRIPTION = open("Readme.md").read()
 
 setup(
     name="so2sql",
