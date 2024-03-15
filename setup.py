fb44707b72390dfe5655c6fcdde8450f3b6e0eab
samuelemwangi <mwangi.s.irungu@gmail.com>
2024-03-16 01:03:44 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
 from setuptools import setup, find_packages
 
+VERSION = "0.0.1"
+DESCRIPTION = "Library to fetch StackOverflow data and persist it to an SQL database"
+LONG_DESCRIPTION = open("README.md").read()
 
 setup(
     name="so2sql",
-    version="0.1",
+    version=VERSION,
+    author="samuelemwangi",
+    author_email="sammiemwangi4@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=[
             "sqlalchemy",
             "stackapi"
             "bs4",
+    ],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     test_suite="tests"
 )
