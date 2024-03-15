52e9166a809b66e7779a199363c25779e96a447e
samuelemwangi <mwangi.s.irungu@gmail.com>
2024-03-16 01:44:40 +0300

    update version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = "Library to fetch StackOverflow data and persist it to an SQL database"
 LONG_DESCRIPTION = """
 Helps to fetch StackOverflow data and persist it to an SQL database. 
